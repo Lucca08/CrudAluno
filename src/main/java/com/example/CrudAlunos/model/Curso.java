6dfaafa9b0b0b0e35cb0f3cc7befcfad3c9f9d9f
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-03-20 11:49:37 -0300

    teste

--- src/main/java/com/example/CrudAlunos/model/Curso.java
+++ src/main/java/com/example/CrudAlunos/model/Curso.java
@@ -1,7 +1,6 @@
 package com.example.CrudAlunos.model;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -12,7 +11,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
